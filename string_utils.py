72b5a697455a0aa3436c96fe79506cab761a8964
amirnujedat2011-cmyk <amirnujedat2011@gmail.com>
2025-11-18 22:18:18 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,6 +1,32 @@
 def split_before_each_uppercases(formula):
-    pass # Replace the `pass` with your code
+    pos = 0
+    parts = []
+    if len(formula) == 0:
+        return []
+    else:
+        for idx in range(1, len(formula)):
+            if formula[idx].isupper():
+                parts.append(formula[pos:idx])
+                pos = idx
+
+    parts.append(formula[pos:])
+    return parts
+
 
 
 def split_at_first_digit(formula):
-    pass # Replace the `pass` with your code
+    digit_pos = -1
+
+    for character in formula[1:]:
+        if character.isdigit():
+            break
+        digit_pos += 1
+
+    digit_pos += 1
+
+    if digit_pos == len(formula):
+        return formula, 1
+    else:
+        text_part = formula[:digit_pos]
+        number_part = int(formula[digit_pos:])
+        return text_part, number_part
