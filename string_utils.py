c1f15f3a0b9d12d287bbc8c3ad4628add59519de
amirnujedat2011-cmyk <amirnujedat2011@gmail.com>
2025-11-19 06:57:17 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,27 +1,33 @@
-def split_before_each_uppercase(formula):
-    if not formula:
+def split_before_each_uppercases(formula):
+    if len(formula) == 0:
         return []
 
     parts = []
-    pos = 0
-    for idx in range(1, len(formula)):
-        if formula[idx].isupper():
-            parts.append(formula[pos:idx])
-            pos = idx
-    parts.append(formula[pos:])
+    start = 0
+
+    for i in range(1, len(formula)):
+        if formula[i].isupper():
+            parts.append(formula[start:i])
+            start = i
+
+    parts.append(formula[start:])
     return parts
 
-def split_at_digit(formula):
-    digit_location = 1   
+
+def split_at_first_digit(formula):
+    digit_location = 1
 
     for ch in formula[1:]:
-        if ch.isdigit():  
+        if ch.isdigit():
             break
-        digit_location += 1   
+        digit_location += 1
 
     if digit_location == len(formula):
-        return formula, 1  
-        
+        return formula, 1
+
     prefix = formula[:digit_location]
     number_part = int(formula[digit_location:])
     return prefix, number_part
+
+
+
